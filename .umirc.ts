9e994125ae986adbb5eabc62a4690c64f60d2abf
JakeQuc <3121894148@qq.com>
2022-08-13 10:43:55 +0800

    feat: update outputPath

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,17 @@ export default defineConfig({
   title: 'art-antd-react',
   favicon: '/icons/logo.ico',
   logo: '/icons/logo.png',
-  outputPath: 'docs-dist',
-  base: '/components/detail-present',
+  outputPath: 'dist',
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   mode: 'site',
   // more config: https://d.umijs.org/config
