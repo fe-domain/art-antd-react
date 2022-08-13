862fdfd720dc93fce2bf437118d05a965538f3c3
JakeQuc <3121894148@qq.com>
2022-08-13 11:19:24 +0800

    rebuild project

---
+++ dist/esm/index.js
@@ -0,0 +1,9 @@
+import 'antd/dist/antd.css';
+export * from "./FormItemsBuilder";
+export * from "./FormGenerator";
+export * from "./DetailPresent";
+export * from "./ListTemplate";
+export * from "./hooks/useRequest";
+export * from "./hooks/useFormModal";
+export * from "./hooks/useConfigListPage";
+export * from 'antd';
